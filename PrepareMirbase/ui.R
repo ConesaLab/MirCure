abf5e8172f1b9afee13fe30c5e02976086ff533c
Tianyuan Liu <59029869+TianYuan-Liu@users.noreply.github.com>
2020-07-04 12:04:19 +0800

    Create ui.R

---
+++ PrepareMirbase/ui.R
@@ -0,0 +1,43 @@
+# Define UI for dataset viewer app ----
+library("shinythemes")
+
+ui <- navbarPage("mirPlot v0",theme = shinytheme("sandstone"),
+
+tabPanel(title='Load Data',
+      # Sidebar layout with a input and output definitions ----
+      sidebarLayout(
+        #position = "right",
+        # Sidebar panel for inputs ----
+        sidebarPanel(
+
+          fileInput("inputfile1", "Mirbase (.gff3)",
+                    multiple = FALSE,
+                    accept = c(".gff3")),
+
+        actionButton("run", "RUN!"),
+        hr(),
+        downloadButton("downloadprecs", "Download Precursors Gff3"),
+        hr(),
+        downloadButton("downloadp5p", "Download 5' arms Gff3"),
+        hr(),
+        downloadButton("download3p", "Download 3' arms Gff3")
+
+
+           ),
+       
+# Main panel for displaying outputs ----
+      mainPanel(
+        h1("Hello world!"),
+        h3("Input data"),
+        tableOutput("visualize"),
+        h3("Precursors"),
+        tableOutput("precs"),
+        h3("5' arms miRNAs"),
+        tableOutput("fiveP"),
+        h3("3'arms miRNAs"),
+        tableOutput("threeP")
+        
+)
+)
+)
+)
